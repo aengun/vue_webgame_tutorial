b5c7381a2e4ff6338825c1995adc970773ef71f1
aengun <aengun@icloud.com>
2021-06-28 23:39:51 +0900

    틱택토+$root, Vue.set, 이벤트버스

---
+++ 7.틱택토/TdComponent.vue
@@ -0,0 +1,26 @@
+lines (19 sloc) 392 Bytes
+
+<template>
+  <td @click="onClickTd">{{ cellData }}</td>
+</template>
+
+<script>
+import EventBus from './EventBus';
+export default {
+  props: {
+    cellData: String,
+    rowIndex: Number,
+    cellIndex: Number,
+  },
+  methods: {
+    onClickTd() {
+      if (this.cellData) return;
+      EventBus.$emit(
+        'clickTd',
+        this.rowIndex,
+        this.cellIndex,
+      );
+    },
+  },
+};
+</script>
---
+++ 8.지뢰찾기/TicTacToe.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <div>당첨 숫자</div>
+    <div></div>
+    <div></div>
+    <button></button>
+  </div>
+</template>
+
+<script>
+export default {
+  components: {},
+  data() {
+    return {};
+  },
+  computed: {},
+  methods: {},
+  mounted() {},
+  beforeDestroye() {},
+  watch: {},
+};
+</script>
+
+<style scoped></style>
